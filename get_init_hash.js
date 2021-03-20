4e1d8de2630e95969f21b2136eaf8cfd8dd4983e
sundong-exideatech <sundong@exidea.tech>
2021-03-20 16:56:35 +0800

    v0.1.3

--- get_init_hash.js
+++ get_init_hash.js
@@ -1,4 +1,4 @@
-var saladPair = require('./build/SaladPair.json');
+var saladPair = require('./build/contracts/SaladPair.json');
 var solidity = require('@ethersproject/solidity');
 
 var INIT_CODE_HASH = solidity.keccak256(['bytes'], [`0x${saladPair.bytecode}`]);
--- test/SaladPair.spec.ts
+++ test/SaladPair.spec.ts
@@ -218,11 +218,8 @@ describe('SaladPair', () => {
 
     const initialPrice = encodePrice(token0Amount, token1Amount)    
     expect(await pair.price0CumulativeLast()).to.eq(initialPrice[0])
-    console.log('-- 1 : ', initialPrice[0]);
     expect(await pair.price1CumulativeLast()).to.eq(initialPrice[1])
-    console.log('-- 2 : ', initialPrice[1]);
     expect((await pair.getReserves())[2]).to.eq(blockTimestamp + 1)
-    console.log('-- 3 : ', blockTimestamp + 1);
 
     const swapAmount = expandTo18Decimals(3)
     await token0.transfer(pair.address, swapAmount)
@@ -231,22 +228,16 @@ describe('SaladPair', () => {
     await pair.swap(0, expandTo18Decimals(1), wallet.address, '0x', overrides) // make the price nice
 
     expect(await pair.price0CumulativeLast()).to.eq(initialPrice[0].mul(10))
-    console.log('-- 4 : ', initialPrice[0].mul(10));
     expect(await pair.price1CumulativeLast()).to.eq(initialPrice[1].mul(10))
-    console.log('-- 5 : ', initialPrice[1].mul(10));
     expect((await pair.getReserves())[2]).to.eq(blockTimestamp + 10)
-    console.log('-- 6 : ', blockTimestamp + 10);
 
     await mineBlock(provider, blockTimestamp + 20)
     await pair.sync(overrides)
 
     const newPrice = encodePrice(expandTo18Decimals(6), expandTo18Decimals(2))
     expect(await pair.price0CumulativeLast()).to.eq(initialPrice[0].mul(10).add(newPrice[0].mul(10)))
-    console.log('-- 7 : ', initialPrice[0].mul(10).add(newPrice[0].mul(10)));
     expect(await pair.price1CumulativeLast()).to.eq(initialPrice[1].mul(10).add(newPrice[1].mul(10)))
-    console.log('-- 8 : ', initialPrice[1].mul(10).add(newPrice[1].mul(10)));
     expect((await pair.getReserves())[2]).to.eq(blockTimestamp + 20)
-    console.log('-- 9 : ', blockTimestamp + 20);
   })
 
   it('feeTo:off', async () => {
